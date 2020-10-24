dff60e71a307a0b2bb1fb029ca911511c2b51b8b
sarvagya8 <sarvagya.8@gmail.com>
2020-10-24 22:20:18 +0530

    WIP

---
+++ IBM/Easy/#205/README.md
@@ -0,0 +1,3 @@
+### Given an integer, find the next permutation of it in absolute order. 
+<br>
+For example, given `48975`, the next permutation would be `49578`.
---
+++ IBM/Easy/#205/solution.js
@@ -0,0 +1,12 @@
+// O() 
+/*---------------- WIP --------------*/
+function nextPermutation(num) {
+    const arr = (num + '').split('').slice(1,)
+    const sorted = arr.sort((a, b) => b - a);
+    let next = 0;
+    for (let i = 0; i < arr.length; i += 1) {
+        // if (arr[0] )
+    }
+/* --------------- WIP --------------*/
+}
+console.log(nextPermutation(48879, 4999))
